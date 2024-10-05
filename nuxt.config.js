ac406accf4802c931ddd28ebe095e7488278f210
spjustice <sanyok.vergun@gmail.com>
2024-10-05 20:43:48 +0400

    config: update config

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,5 +1,6 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
-export default defineNuxtConfig({
+const config = defineNuxtConfig({
+  buildModules: ['@nuxtjs/svg-sprite'],
   compatibilityDate: '2024-04-03',
   devtools: { enabled: true },
   modules: [
@@ -15,3 +16,4 @@ export default defineNuxtConfig({
     ],
   ],
 })
+export default config
